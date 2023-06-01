d33451bedf2fdf09f0389e9305ab667d991ae30e
Adam Harvey <heyitsmeharv@gmail.com>
2023-06-01 01:49:17 +0100

    rendering game object

---
+++ Game.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <SDL.h> 
+#include <SDL_image.h>
+#include <stdio.h>
+
+
+class Game {
+public:
+	Game();
+	~Game();
+
+	void init(const char* title, int xpos, int ypos, int width, int height, bool fullscreen);
+
+	void handleEvents();
+	void update();
+	void render();
+	void clean();
+
+	bool running() { return isRunning; };
+
+private:
+	int cnt = 0;
+	bool isRunning = false;
+	SDL_Window* window = nullptr;
+	SDL_Renderer* renderer = nullptr;
+};
--- main.cpp
+++ main.cpp
@@ -1,15 +1,34 @@
-#include <SDL.h> 
-#include <iostream>  
+#include "Game.h"
 
-int main(int argc, char* args[]) {     
-	// The window we'll be rendering to    
-	SDL_Window* window = NULL;    
-	// The surface contained by the window   
-	SDL_Surface* screenSurface = NULL;     
-	// Initialize SDL 
-	if (SDL_Init(SDL_INIT_VIDEO) < 0)     { 
-		printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());  
-	}      
+Game *game = nullptr;
+
+int main(int argc, char* args[]) {
+
+	const int FPS = 60;
+	const int frameDelay = 1000 / FPS;
+
+	Uint32 frameStart;
+	int frameTime;
+
+	game = new Game();
+
+	game->init("PROJECT HANKOOK", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, false);
+
+	while (game->running()) {
+		frameStart = SDL_GetTicks();
+
+		game->handleEvents();
+		game->update();
+		game->render();
+
+		frameTime = SDL_GetTicks() - frameStart;
+
+		if (frameDelay > frameTime) {
+			SDL_Delay(frameDelay - frameTime);
+		}
+	}
+
+	game->clean();
 	
-	return 0; 
+	return 0;
 }
